7089ac66f2ad7ebdf9b4b20a11a883da75ba6742
liuggio <liuggio@yahoo.it>
2011-04-14 17:49:40 +0100

    Updated the functions

--- backupMysqlEnv.sh
+++ backupMysqlEnv.sh
@@ -3,6 +3,7 @@
 # load config
 MY_DIR=`dirname $0`
 source $MY_DIR/config.conf
+source $MY_DIR/parameterFunctions.sh
 if [ -z "$CONFIG_LOADED" ]; then
     echo "error on loading file";
     exit
@@ -26,27 +27,42 @@ DEST_PASSWORD=$DEV_PASSWORD
 # function that print the help
 needHelp() {
 	echo "usage:"
+    echo "      --help or -h";
+    echo ""
 	echo "      $0 copy:structure [from [to]]";
     echo "        eg. copy:structure test dev";
+    echo ""
 	echo "      $0 copy:data";
     echo "        eg. copy:data test dev";
+    echo ""
 	echo "      $0 show:tables [from]";
     echo "        eg. show:tables test";
+    echo ""
 	echo "      $0 count:tables [from]";
     echo "        eg. count:tables test";
+    echo ""
     echo "      $0 gzip:data [from]";
     echo "        eg. gzip:data live";
+    echo ""
     echo "      $0 gzip:structure [from]";
-    echo "        eg. gzip:structure live";               
+    echo "        eg. gzip:structure live";
+    echo ""
+    echo "options"
+    echo "       --exclude-from=\"FILE\""
+    echo "       -e=\"FILE\""
+    echo ""
+    echo "       --include-from=\"FILE\""
+    echo "       -i=\"FILE\""
+    echo ""
+
 }
 
 importTables() {
     COUNTER=0;
-    echo "import $CREATE_IMPORT_TABLES_FILE";
     if [ "$CREATE_IMPORT_TABLES_FILE" -eq "1" ]; then
         echo "Populate List Tables ..."; 
-        echo "{"
-        echo "" > $IMPORT_TABLES_FILE;
+        echo -n "{ "
+        echo -n "" > $IMPORT_TABLES_FILE;
         # populate the file with all the tables
         echo show tables | mysql --host=$SOURCE_HOST  --user=$SOURCE_USERNAME --password=$SOURCE_PASSWORD --database=$SOURCE_DATABASE | while read Table
         do
@@ -56,6 +72,7 @@ importTables() {
                 echo "$Table" >> $IMPORT_TABLES_FILE
             fi
         done
+        echo ""
         echo -n "}"
         echo "the database has `cat $IMPORT_TABLES_FILE | wc -l` tables";
     fi
@@ -67,8 +84,7 @@ showTables() {
 }
 
 countTables() {
-    importTables;
-    cat $IMPORT_TABLES_FILE | wc -l;   
+    importTables;   
 }
 
 removeFirstLine() {
@@ -136,11 +152,9 @@ createCommandGzipTableData() {
 # $1 contain the function to call
 executeDump() {
     # executing mysqldump
-    COUNTER=0;
-    ERRORCOUNTER=0;
-    SUCCESSCOUNTER=0;
+  
     START=$(date +%s)
-
+    ERRORCOUNTER=0
     if [ -z "$1" ]; then
         return 1;
     else
@@ -159,100 +173,43 @@ executeDump() {
             eval $COMMAND
             #fail?
             if [ "$?" -ne "0" ]; then
+                echo "-fail $tableName"
                 ERRORCOUNTER=`expr $ERRORCOUNTER + 1`;
             else
+                echo "+success $tableName"
                 SUCCESSCOUNTER=`expr $SUCCESSCOUNTER + 1`;
             fi         
              
             ENDtmp=$(date +%s)
             DIFFtmp=$(( $ENDtmp - $STARTtmp ))
-            echo "consumed:[$DIFFtmp] seconds"; 
-            echo "}"
+            
+            echo "$DIFFtmp sec } total Success:[$SUCCESSCOUNTER], Error:[$ERRORCOUNTER]";
         done
     fi
     END=$(date +%s)
     DIFF=$(( $END - $START ))
-
-    echo "ERRORS: [$ERRORCOUNTER], OK: [$SUCCESSCOUNTER]";
-    echo "consumed:[$DIFF] seconds";
+    echo "finished, consumed:[$DIFF] seconds";
 }
 
 
-if [ -z "$2" ]; then
-    echo "using TEST as Source";
-    
-elif [ $2 = "live" ]; then
-    SOURCE_HOST=$LIVE_HOST
-    SOURCE_DATABASE=$LIVE_DATABASE
-    SOURCE_USERNAME=$LIVE_USERNAME
-    SOURCE_PASSWORD=$LIVE_PASSWORD
-    
-elif [ $2 = "test" ]; then
-    SOURCE_HOST=$TEST_HOST
-    SOURCE_DATABASE=$TEST_DATABASE
-    SOURCE_USERNAME=$TEST_USERNAME
-    SOURCE_PASSWORD=$TEST_PASSWORD
-    
-elif [ $2 = "dev" ]; then    
-    SOURCE_HOST=$DEV_HOST
-    SOURCE_DATABASE=$DEV_DATABASE
-    SOURCE_USERNAME=$DEV_USERNAME
-    SOURCE_PASSWORD=$DEV_PASSWORD
-    
-else
-    echo "error use live|test|dev"
-fi
-
-
-if [ -z "$3" ]; then
-    echo "";
-    
-elif [ $3 = "live" ]; then
-    echo "error is not possible to use LIVE as destination";
-    
-elif [ $3 = "test" ]; then
-    DEST_HOST=$TEST_HOST
-    DEST_DATABASE=$TEST_DATABASE
-    DEST_USERNAME=$TEST_USERNAME
-    DEST_PASSWORD=$TEST_PASSWORD
  
-elif [ $3 = "dev" ]; then    
-    DEST_HOST=$DEV_HOST
-    DEST_DATABASE=$DEV_DATABASE
-    DEST_USERNAME=$DEV_USERNAME
-    DEST_PASSWORD=$DEV_PASSWORD
-    
-fi
 
-
-
-if [ -z "$1" ]; then
-    needHelp
-elif [ $1 = "copy:structure" ]; then
-    echo "copy structure"
+copyStructure() {
     importTables
     removeFirstLine
     ignoreTables   
     executeDump createCommandCopyTableStructureFromSourceToDest
-                
-elif [ $1 = "copy:data" ]; then
-   echo "copy data"
+}
+     
+copyData() {
    importTables
    removeFirstLine
    ignoreTables
    executeDump createCommandCopyTableFromSourceToDest
-   
-elif [ $1 = "show:tables" ]; then
-    echo "showTables";
-    showTables
-
-elif [ $1 = "count:tables" ]; then
-    echo "countTables";
-    countTables
+}
+ 
     
-elif [ $1 = "gzip:data" ]; then
-    echo "gzipping data";
-
+gzipData() {
     importTables
     removeFirstLine
     ignoreTables
@@ -261,25 +218,47 @@ elif [ $1 = "gzip:data" ]; then
     echo "backup into $BACKUP_DIR"
  
     mkdir $BACKUP_DIR && executeDump createCommandGzipTableData
-    
-elif [ $1 = "gzip:structure" ]; then
-    echo "gzipping structure";
+}
 
+gzipStructure() {
     importTables
     removeFirstLine
     ignoreTables
     
     BACKUP_DIR=$BACKUP_DIR/`date +"%b-%d-%y_%H-%m-%s"`
     echo "backup into $BACKUP_DIR"
- 
     mkdir $BACKUP_DIR && executeDump createCommandGzipTableStructure
-    
-else
+}
+
+
+
+#usage
+# parameter --exclude-from=FILE
+#           --include-from=FILE
+# only with gzip:...
+#           --backup-folder=directory
+#       
+#
+
+
+handleParameter $@
+
+if [ -z "$functionAction" ]; then
+    echo "error on parameters"
     needHelp
+    exit 1
 fi
-                   
-       
-  
+
+setSource $fromEnv
+setDest $toEnv
+
+printVars
+
+echo "executing $functionAction"
+eval $functionAction
+echo "bye"
+exit 0;
+
                                                          
                                                            
                                                            
